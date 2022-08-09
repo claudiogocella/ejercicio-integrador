5cac28a0c5d0e8e095e8d489d4786fd27a4b5049
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-08-09 16:19:47 -0300

    agregu{é links agregué dos links de posibles noticias

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 let parrafo = document.getElementById("resultado");
 let boton = document.getElementById("boton");
 let input = document.getElementById(entrada); 
-let descuento : string = "MI CODIGO"
+let descuento : string = "MICODIGO"
 
 function mostrarResultado() {
   let result : string input.value;  //acá tomo el resultado del input
-  //entonces pregunto: 
-  if (result === descuento){
-    
-  }
-  parrafo?.innerHTML = "Este es mi resutado";
-  
-}
+  //entonces pregunto: = esta es la lógica para preguntar si el precio es mayor a $2500
+  if (result === descuento) {
+    parrafo?.innerHTML = "Usted tiene descuento";  
+  } else {
+    parrafo?.innerHTML = "El código es incorrecto";
+    }
+} 
 //agregar evento al botón click:
 boton?.addEventListener("click", mostrarResultado);
+
+
+
+
+
+
+
+
+/* DE ESTA NOTICIA SE PUEDE DESPRENDER EL LINK PARA CARRITO COMPRAS
+https://www.clarin.com/sociedad/dormir-desayunar-falso-dilema-moderno-atenta-salud_0_fK2LlnVXMc.html
+*/
+
+/* https://gastronomiaycia.republica.com/2022/08/01/nueva-etiqueta-alimentaria-que-mide-la-temperatura-y-el-tiempo/ */  
